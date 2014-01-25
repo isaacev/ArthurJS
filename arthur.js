a4067dde0f7d64b3eaa04b09ac8915877b3bb87f
Isaac Evavold <isaacevavold@gmail.com>
2014-01-24 23:38:07 -0600

    tentative fix to hanging else

--- arthur.js
+++ arthur.js
@@ -1,75 +1,17 @@
-'use strict';
-
 var Fs = require('fs');
 var Jison = require('jison').Parser;
 
-//var grammar = require('./src/grammar.js').grammar;
 var grammar = require('./src/grammar.js').grammar;
-var lexerLib = require('./src/lexer.js').lexer;
-var i = -1;
-var tokens = ['IF', '(', 'IDENTIFIER', ')', ':', 'TERMINATOR', 'IND', 'IDENTIFIER', '(', ')', 'TERMINATOR', 'DED', 'ELSE', ':', 'TERMINATOR', 'IND', 'IDENTIFIER', '(', ')', 'TERMINATOR', 'DED', 'TERMINATOR'];
-var literal = ['if', '(', 'bar', ')', ':', false, false, 'foobar', '(', ')', false, false, 'else', ':', false, false, 'foobar', '(', ')', false, false, false];
-
-function lexer() {
-	this.setInput = function (input) {
-		return this;
-	};
-	this.lex = function () {
-		i++;
-		if (literal[i] !== false) {
-			this.yytext = literal[i];
-		}
-		return tokens[i];
-	};
-};
+var lexer = require('./src/lexer2.js').lexer;
 var yy = require('./src/yy.js').yy;
 
 var Parser = new Jison(grammar);
 Parser.lexer = new lexer();
 Parser.yy = yy;
 
-function loadFile(path, callback) {
-	callback = callback || function () {};
-
-	Fs.readFile(path, 'utf8', function (err, data) {
-		if (err) {
-			throw 'File Error: ' + err.toString();
-		} else {
-			callback(data);
-		}
-	});
-}
-
-loadFile('./test.arthur', function (data) {
-	var tokenize = false;
-	if (!tokenize) {
-		console.log(Parser.parse(data + '\n').compile({
-			header: false,
-			bare: true
-		}));
-	} else {
-		lexer.setInput(data);
-		console.log(lexer.lex());
-		console.log(lexer.lex());
-		console.log(lexer.lex());
-		console.log(lexer.lex());
-		console.log(lexer.lex());
-		console.log(lexer.lex());
-		console.log(lexer.lex());
-		console.log(lexer.lex());
-		console.log(lexer.lex());
-		console.log(lexer.lex());
-		console.log(lexer.lex());
-		console.log(lexer.lex());
-		console.log(lexer.lex());
-		console.log(lexer.lex());
-		console.log(lexer.lex());
-		console.log(lexer.lex());
-		console.log(lexer.lex());
-		console.log(lexer.lex());
-		console.log(lexer.lex());
-		console.log(lexer.lex());
-		console.log(lexer.lex());
-		console.log(lexer.lex());
-	}
+Fs.readFile('./test.arthur', 'utf8', function (err, data) {
+	console.log(Parser.parse(data + '\n').compile({
+		bare: false,
+		header: true
+	}));
 });
--- src/lexer.js
+++ src/lexer.js
@@ -1,106 +1,176 @@
 'use strict';
 
-var Lexer = require('lex');
-
-var row = 1;
-var col = 1;
 var indent = [0];
 
-var ongoing = [];
+var rules = {
+	space: [
+		/^ +/,
+		function (raw) {
+			return false;
+		}
+	],
+	terminator: [
+		/^\n+/,
+		function (raw, lex, increment) {
+			var indentation = /^\t*/.exec(lex.remaining.substr(1))[0].length;
+			increment(indentation);
+			var tokens = ['TERMINATOR'];
 
-var lexer = exports.lexer = new Lexer(function (char) {
-	throw new Error('Unexpected character at row ' + row + ', col ' + ': "' + char + '"');
-});
+			if (indentation > indent[0]) {
+				// indentation level increased
+				indent.unshift(indentation);
+				tokens.push('IND');
+			} else if (indentation < indent[0]) {
+				// indentation level decreased
+				while (indentation < indent[0]) {
+					tokens.push('DED');
+					tokens.push('TERMINATOR');
+					indent.shift();
+				}
+			}
 
-// ignore spaces
-lexer.addRule(/ +/, function (raw) {
-	col += raw.length;
-});
+			//console.log(tokens);
+			return tokens;
+		}
+	],
+	identifier: [
+		/^[a-z]+/,
+		function (raw, lex) {
+			if (RESERVED_AR.indexOf(raw) > -1 || RESERVED_JS.indexOf(raw) > -1) {
+				// reserved ARTHUR or JS word
+				if (raw === 'else' && lex.tokens[lex.tokens.length - 1] === 'TERMINATOR') {
+					lex.tokens.pop();
+					lex.literals.pop();
+				}
+				return raw.toUpperCase();
+			} else {
+				return 'IDENTIFIER';
+			}
+		}
+	],
+	logicSymbol: [
+		/^\>\=|^\<\=|^\=\=|^\>|^\</,
+		function () {
+			return 'LOGIC';
+		}
+	],
+	literalSymbol: [
+		/^\+\+|^\+|^\-\-|^\.\.|^\.|^\-|^\/|^\*\*|^\*|^\,|^\(|^\)|^\[|^\]|^\{|^\}|^\=|^\:|^\?|^\@/,
+		function (raw) {
+			if (['+', '-', '*', '/', '**'].indexOf(raw) > -1) {
+				return 'MATH';
+			} else {
+				return raw;
+			}
+		}
+	],
+	string: [
+		/^\'([^\']*)\'/,
+		function () {
+			return 'STRING';
+		}
+	],
+	number: [
+		/^\-?[0-9]+(?:\.[0-9]+)?/,
+		function () {
+			return 'NUMBER';
+		}
+	],
+	unexpected: [
+		/^./,
+		function (raw, lex) {
+			throw Error('Unexpected "' + raw + '"');
+		}
+	]
+};
 
-// process indent and dedent
-lexer.addRule(/^\t*/gm, function (raw) {
-	var indentation = raw.length;
-
-	col += indentation;
-
-	if (indentation > indent[0]) {
-		indent.unshift(indentation);
-		return 'IND';
-	}
-
-	var tokens = [];
-
-	while (indentation < indent[0]) {
-		tokens.push('DED');
-		tokens.push('TERMINATOR');
-		indent.shift();
-	}
-
-	if (tokens.length) {
-		return tokens;
-	}
-});
-
-// comments
-lexer.addRule(/\#[^\n]*/, function (raw) {
-	row += raw.length;
-	return 'COMMENT';
-});
-
-// terminators
-lexer.addRule(/\n+/, function (raw) {
-	row += raw.length;
-	return 'TERMINATOR';
-});
-
-// identifier or keyword
-lexer.addRule(/[a-zA-Z]+/, function (raw) {
-	col += raw.length;
-	this.yytext = raw;
-
-	var out;
-	if (RESERVED_AR.indexOf(raw) > -1 || RESERVED_JS.indexOf(raw) > -1) {
-		// reserved ARTHUR or JS word
-		return raw.toUpperCase();
-	} else {
-		return 'IDENTIFIER';
-	}
-});
-
-// logical symbols
-lexer.addRule(/\>\=|\<\=|\=\=|\>|\</, function (raw) {
-	col += raw.length;
-	this.yytext = raw;
-
-	return 'LOGIC';
-});
-
-// literal symbols
-lexer.addRule(/\+\+|\+|\-\-|\.\.|\.|\-|\/|\*\*|\*|\,|\(|\)|\[|\]|\{|\}|\=|\:|\?|\@/, function (raw) {
-	col += raw.length;
-	this.yytext = raw;
-
-	if (['+', '-', '*', '/', '**'].indexOf(raw) > -1) {
-		return 'MATH';
-	} else {
-		return raw;
-	}
-});
-
-// strings
-lexer.addRule(/\'([^\']*)\'/, function (raw, unquoted) {
-	col += raw.length;
-	this.yytext = unquoted;
-	return 'STRING';
-});
-
-// numbers
-lexer.addRule(/\-?[0-9]+(?:\.[0-9]+)?/, function (raw) {
-	col += raw.length;
-	this.yytext = raw;
-	return 'NUMBER';
-});
-
+var WHITESPACE = ['TERMINATOR', 'IND', 'DED'];
 var RESERVED_AR = ['def', 'as', 'elseif'];
 var RESERVED_JS = ['true', 'false', 'if', 'else', 'return', 'for', 'while', 'in'];
-var BLOCKING = ['def', 'if', 'else', 'for', 'while'];
+
+function check(lex, rule) {
+	var match = rule[0].exec(lex.remaining);
+	if (match !== null) {
+		// advance the index
+		var advancement = match[0].length;
+
+		var result = rule[1](match[0], lex, function (increment) {
+			advancement += increment;
+		});
+		if (result !== false) {
+			if (result instanceof Array) {
+				// multiple tokens returned
+				for (var i = 0, len = result.length; i < len; i++) {
+					lex.literals.push(false);
+					lex.tokens.push(result[i]);
+				}
+			} else {
+				if (WHITESPACE.indexOf(result) > -1) {
+					lex.literals.push(false);
+				} else {
+					lex.literals.push(match[0]);
+				}
+				lex.tokens.push(result);
+			}
+		}
+		return advancement;
+	} else {
+		return false;
+	}
+}
+
+function Lexer() {
+	this.tokens = [];
+	this.literals = [];
+	this.remaining;
+	this.index = 0;
+	this.setInput = function (input) {
+		this.input = input;
+		this.remaining = input;
+	};
+	this.lex = function () {
+		while (this.remaining = this.input.slice(this.index)) {
+			// check(this, rules[i]);
+			// returns `undefined` or `false`
+
+			// returns `number` or `false`
+			this.index += check(this, rules.space) ||
+				check(this, rules.terminator) ||
+				check(this, rules.identifier) ||
+				check(this, rules.logicSymbol) ||
+				check(this, rules.literalSymbol) ||
+				check(this, rules.string) ||
+				check(this, rules.number) ||
+				check(this, rules.unexpected);
+		}
+		return {
+			tokens: this.tokens,
+			literals: this.literals
+		};
+	};
+}
+
+exports.lexer = function () {
+	var scanner = new Lexer();
+	var tokens, literals;
+	var i = -1;
+	this.setInput = function (input, returnQuick) {
+		scanner.setInput(input);
+		var results = scanner.lex();
+		tokens = results.tokens;
+		literals = results.literals;
+		if (returnQuick === true) {
+			console.log(tokens);
+			return;
+		}
+		return this;
+	};
+
+	this.lex = function () {
+		i++;
+		if (literals[i] !== false) {
+			this.yytext = literals[i];
+		}
+		return tokens[i];
+	}
+};
