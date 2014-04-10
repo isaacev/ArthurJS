927166ec2c30239241145b69ae3862bdfb1946a5
Isaac Evavold <isaacevavold@gmail.com>
2014-04-09 19:07:10 -0500

    tests modulo, += and explicit tokens

--- tests/tests.js
+++ tests/tests.js
@@ -6,11 +6,20 @@ exports.test = function (describe) {
 		test('divide', 3, '12 / 4');
 		test('parentheses', 10, '4 + (3 * 2)');
 		test('exponent', 16, '4 ** 2');
+		test('modulo', 2, '10 % 4');
+		test('+=', 6, 'foo = 2\nfoo += 4');
+		test('-=', 0, 'foo = 2\nfoo -= 2');
+		test('*=', 8, 'foo = 2\nfoo *= 4');
+		test('/=', 1, 'foo = 2\nfoo /= 2');
 	});
 
 	describe('string manipulation', function (test) {
 		test('concatenation', 'foobar', "'foo' + 'bar'");
 		test('length', 10, "'abcdefghij'.length");
+	});
+
+	describe('explicit', function (test) {
+		test('number', 42, '`42`');
 	});
 
 	describe('arrays', function (test) {
